abbc002bad5699c87a9070facde3bd6f727fc957
Dominick Blue <dominick.blue7@gmail.com>
2024-10-03 18:58:49 -0700

    Implementing a new hashamp algorithm

--- src/week1/HashMapExample.java
+++ src/week1/HashMapExample.java
@@ -1,73 +1,39 @@
 package week1;
 
 import java.util.HashMap;
-
-
-//public class HashMapExample
-//{
-//
-//    private static HashMap<Character, Integer> charCount(String s)
-//    {
-//        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
-//
-//        for (char c : s.toCharArray())
-//        {
-//            if (count.get(c) == null)
-//            {
-//                count.put(c, 0);
-//            }
-//
-//            count.put(c, count.get(c) + 1);
-//        }
-//
-//        return count;
-//    }
-//
-//
-//    public static boolean anagrams(String s1, String s2)
-//    {
-//        return charCount(s1).equals(charCount(s2));
-//    }
-//
-//    public static void main(String[] args)
-//    {
-//        String s1 = "dominick";
-//        String s2 = "dominique";
-//
-//        System.out.println(anagrams(s1, s2));
-//    }
-//}
-
 public class HashMapExample
 {
 
     public static HashMap<Character, Integer> charCount(String s)
     {
-        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> count = new HashMap<>();
 
-        for (char c : s.toCharArray())
-        {
-            if (count.get(c) == null)
-            {
-                count.put(c, 0);
-            }
 
-            count.put(c, count.get(c) + 1);
-        }
 
         return count;
     }
 
-    public static boolean anagrams(String s1, String s2)
+    public static char mostFrequentCharacter(String s1)
     {
-        return charCount(s1).equals(charCount(s2));
+        HashMap<Character, Integer> count = new HashMap<>();
+        char mostFrequent = '\0';
+
+        for (char c : s1.toCharArray())
+        {
+            if (mostFrequent == '\0')
+        }
+
+
+
+        return mostFrequent;
     }
 
     public static void main(String[] args)
     {
-        String s1 = "Justin";
-        String s2 = "Dustin";
 
-        System.out.println(anagrams(s1, s2));
+        String s1 = "mississippi";
+
+        System.out.println("Most frequent char: " + mostFrequentCharacter(s1));
+
     }
 }
