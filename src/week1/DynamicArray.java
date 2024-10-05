dce53f0885ce4f0b4a5d7c662f2c493c46594d36
Dominick Blue <dominick.blue7@gmail.com>
2024-10-05 05:35:01 -0700

    Implementing a dynamic array

---
+++ src/week1/DynamicArray.java
@@ -0,0 +1,69 @@
+package week1;
+
+public class DynamicArray
+{
+    /*
+    Design a Dynamic Array (aka a resizable array) class, such as an ArrayList in Java or a vector in C++.
+
+    Your DynamicArray class should support the following operations:
+
+    DynamicArray(int capacity) will initialize an empty array with a capacity of capacity, where capacity > 0.
+    int get(int i) will return the element at index i. Assume that index i is valid.
+    void set(int i, int n) will set the element at index i to n. Assume that index i is valid.
+    void pushback(int n) will push the element n to the end of the array.
+    int popback() will pop and return the element at the end of the array. Assume that the array is non-empty.
+    void resize() will double the capacity of the array.
+    int getSize() will return the number of elements in the array.
+    int getCapacity() will return the capacity of the array.
+
+    If we call void pushback(int n) but the array is full, we should resize the array first.
+    * */
+
+    private int[] arr;
+    private int length;
+    private int capacity;
+
+    // Constructor to initialize the dynamic array
+    public DynamicArray(int capacity)
+    {
+        this.capacity = capacity;
+        this.length = 0;
+        this.arr = new int[this.capacity];
+    }
+
+    // Get value at the i-th index
+    public int get(int i)
+    {
+
+    }
+
+    public void set(int i, int n)
+    {
+
+    }
+
+    public void pushback(int n)
+    {
+
+    }
+
+    public int popback()
+    {
+
+    }
+
+    private void resize()
+    {
+
+    }
+
+    public int getSize()
+    {
+
+    }
+
+    public int getCapacity()
+    {
+
+    }
+}
