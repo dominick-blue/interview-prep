52165e429d5e84ba3183c8768385f5a20849ca48
Dominick Blue <dominick.blue7@gmail.com>
2024-10-06 08:52:17 -0700

    Building intuition for the hashmap lookup pattern

--- src/week1/SolutionGroupAnagrams.java
+++ src/week1/SolutionGroupAnagrams.java
@@ -1,40 +1,72 @@
 package week1;
 
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 public class SolutionGroupAnagrams
 {
+    // Brute Force Solution
+
+//    public List<List<String>> groupAnagrams(String[] strs)
+//    {
+//        Map<String, List<String>> res = new HashMap<>();
+//
+//        for (String s : strs)
+//        {
+//            int[] count = new int[26];
+//
+//            for (char c : s.toCharArray())
+//            {
+//                count[c - 'a']++;
+//            }
+//
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < 26; i++)
+//            {
+//                sb.append('#');
+//                sb.append(count[i]);
+//            }
+//            String key = sb.toString();
+//
+//            if (!res.containsKey(key))
+//            {
+//                res.put(key, new ArrayList<>());
+//            }
+//            res.get(key).add(s);
+//        }
+//
+//        return new ArrayList<>(res.values());
+//    }
+
+    // Optimal Solution O(m * n)
+
+    /*
+    * Approach
+    *
+    * 1. Create a map (k, v) where the keys are a string and the values are a list of strings
+    * 2. Create a for loop to iterate over each word string in the list of strings
+    * 3. For each string, create a char list, the sort that list, then create a new String object with that list of sorted chars
+    * 4. Use the locate and update pattern by checking to see if a sorted word is not in the map. If it is not, add it along with a new
+    * array list.
+    * 5. Get the sorted word key and add the word into its list value
+    * 6. Once complete with the loop, return the map.
+    * */
     public List<List<String>> groupAnagrams(String[] strs)
     {
-        Map<String, List<String>> res = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
-        for (String s : strs)
+    for (String word : strs)
         {
-            int[] count = new int[26];
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
 
-            for (char c : s.toCharArray())
+            if (!map.containsKey(sortedWord))
             {
-                count[c - 'a']++;
+                map.put(sortedWord, new ArrayList<>());
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 26; i++)
-            {
-                sb.append('#');
-                sb.append(count[i]);
-            }
-            String key = sb.toString();
-
-            if (!res.containsKey(key))
-            {
-                res.put(key, new ArrayList<>());
-            }
-            res.get(key).add(s);
+            map.get(sortedWord).add(word);
         }
-
-        return new ArrayList<>(res.values());
+        return new ArrayList<>(map.values());
     }
 }
